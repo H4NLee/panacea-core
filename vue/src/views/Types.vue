087fc9e1980723b39d9a041d4e54fd2bd0f307df
Youngjoon Lee <yjlee@medibloc.org>
2021-06-03 16:07:48 +0900

    Scaffold types

--- vue/src/views/Types.vue
+++ vue/src/views/Types.vue
@@ -2,6 +2,10 @@
 	<div>
 		<div class="container">
       	<!-- this line is used by starport scaffolding # 4 -->
+		<SpType modulePath="medibloc.panaceacore.aol" moduleType="Owner"  />
+		<SpType modulePath="medibloc.panaceacore.aol" moduleType="Record"  />
+		<SpType modulePath="medibloc.panaceacore.aol" moduleType="Writer"  />
+		<SpType modulePath="medibloc.panaceacore.aol" moduleType="Topic"  />
 		</div>
 	</div>
 </template>
